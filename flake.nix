68ae61c46abe47cd91bef792e651b210a580fc3f
YvesDonato <yvesdonato0@gmail.com>
2024-05-14 08:41:55 -0400

    changes

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
     };
 
     # Hyprland
-    hyprland.url = "github:hyprwm/Hyprland";
+    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
 
     # nix-colors
     nix-colors.url = "github:misterio77/nix-colors";
