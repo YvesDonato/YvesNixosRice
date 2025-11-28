3a5c0642757020adc51f6f575e503939b4c2304d
YvesDonato <yvesdonato@pm.me>
2025-11-28 09:29:10 -0500

    test

--- homeManager/modules/scripts/rofi/codebox.sh
+++ homeManager/modules/scripts/rofi/codebox.sh
@@ -1,1 +1,14 @@
+#!/usr/bin/env bash
+
+select=$(printf "Turn Off\nReboot\nLock" |rofi -dmenu -no-show-icons -theme-str 'inputbar { enabled: false; } listview {lines: 3;}')
+
+case $select in "Turn Off")
+poweroff
+;;
+"Reboot")
+reboot
+;;
+"Lock")
+hyprlock
+esac
 
